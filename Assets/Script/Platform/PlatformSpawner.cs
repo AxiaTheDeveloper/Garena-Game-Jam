04b8805a65850c14eb3e56b0d93f95c59290945b
AxiaTheDeveloper <101692512+AxiaTheDeveloper@users.noreply.github.com>
2023-12-09 11:50:13 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Script/Platform/PlatformSpawner.cs
+++ Assets/Script/Platform/PlatformSpawner.cs
@@ -8,6 +8,7 @@ public class PlatformSpawner : MonoBehaviour
     [SerializeField]private GameObject[] platformsPrefab;
     [SerializeField]private float heightNow, heightAdd;
     [SerializeField]private int startHeight, totalHeight, lastRandomNumber;
+    [SerializeField]private GameObject parent;
     private void Awake() 
     {
         Instance = this;
@@ -19,17 +20,40 @@ public class PlatformSpawner : MonoBehaviour
     {
         for(int i=0;i<totalSpawn;i++)
         {
-            int random = Random.Range(0,2);
+            int maxLength = 0, minLength = 0;
+            if(totalHeight/10 == 0)
+            {
+                minLength = 0;
+                maxLength = 9;
+            }
+            else if(totalHeight/10 == 1)
+            {
+                minLength = 9;
+                maxLength = 18;
+            }
+            else if(totalHeight/10 == 2)
+            {
+                minLength = 4;
+                maxLength = platformsPrefab.Length;
+            }
+            int random = Random.Range(minLength,maxLength);
             if(i > 0)
             {
-                while(random == lastRandomNumber)
+                while(random == lastRandomNumber )
                 {
-                    random = Random.Range(0,2);
+                    random = Random.Range(minLength,maxLength);
+                    while(lastRandomNumber == 5 && random == 7)
+                    {
+                        random = Random.Range(minLength,maxLength);
+                    }
                 }
+                
             }
             lastRandomNumber = random;
-            Transform newPlatform = Instantiate(platformsPrefab[random].transform, this.gameObject.transform);
-            newPlatform.transform.position = new Vector3(transform.position.x, heightNow, transform.position.z);
+            Transform newPlatform = Instantiate(platformsPrefab[random].transform, parent.gameObject.transform);
+            // Debug.Log(newPlatform);
+            newPlatform.transform.localPosition = new Vector3(transform.position.x, heightNow, transform.position.z);
+            // Debug.Log(newPlatform.transform.localPosition);
             heightNow += heightAdd;
             totalHeight++;
             newPlatform.GetComponent<PlatformIdentity>().ChangePlatformHeight(totalHeight);
