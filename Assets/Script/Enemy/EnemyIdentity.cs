140cf1bfce54ec4d1bfa05c01d11c45b63ea2d33
CloterChohan <72092638+CloterChohan@users.noreply.github.com>
2023-12-09 16:09:43 +0700

    COPWAOPSODWASDWASD

--- Assets/Script/Enemy/EnemyIdentity.cs
+++ Assets/Script/Enemy/EnemyIdentity.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class EnemyIdentity : MonoBehaviour
 {
+    ParticleSystem WalkingGibsPE;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = gameObject.transform.GetChild(0).gameObject.transform.GetComponent<Animator>();
+    }
+
     public void Suicide()
+    {
+        animator.Play("Die");
+    }
+
+    public void YesPleaseDie()
     {
         Destroy(this.gameObject);
     }
-
 }
